0da9f073c4ffb6069ca36156b476cab053daa34c
Tosone <i@tosone.cn>
2020-10-14 22:43:03 +0800

    :heavy_minus_sign: Remove sqlite deps.

--- database/db.go
+++ database/db.go
@@ -1,12 +1,26 @@
-package database
+package v2
 
 import (
 	"encoding/hex"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
+	"encoding/json"
 	"hash/fnv"
 	"strconv"
 	"time"
+
+	"github.com/dgraph-io/badger/v2"
+	"github.com/tosone/logging"
+)
+
+// Status ..
+type Status string
+
+const (
+	// DoneStatus ..
+	DoneStatus Status = "done"
+	// ErrorStatus ..
+	ErrorStatus Status = "error"
+	// PendingStatus ..
+	PendingStatus Status = "pending"
 )
 
 type Content struct {
@@ -20,54 +34,153 @@ type Content struct {
 	Region         string `json:"region"`
 	Endpoint       string `json:"endpoint"`
 	Force          bool   `json:"force"`
+	Progress       string `json:"progress"`
+	Status         Status `json:"status"`
+	Message        string `json:"message"`
+	//Content []byte `json:"-"`
+}
 
-	Progress string `json:"progress"`
+var DBEngine *badger.DB
 
-	Status  Status `json:"-"`
-	Message string `json:"-"`
-	Content []byte `json:"-"`
+const databaseFile = "badgerDir"
+
+func init() {
+	var err error
+	if DBEngine, err = badger.Open(badger.DefaultOptions(databaseFile)); err != nil {
+		logging.Fatal(err)
+	}
+}
+
+// Teardown ..
+func Teardown() (err error) {
+	if err = DBEngine.Close(); err != nil {
+		return
+	}
+	return
 }
 
 // Insert ..
 func (t *Content) Insert() (err error) {
-	if err = DBEngine.Debug().Create(t).Error; err != nil {
+	var txn = DBEngine.NewTransaction(true)
+
+	var data []byte
+	if data, err = json.Marshal(*t); err != nil {
+		return
+	}
+
+	logging.Info(t)
+
+	var entry = badger.NewEntry([]byte(t.Name), data)
+	if err = txn.SetEntry(entry); err != nil {
+		return
+	}
+	if err = txn.Commit(); err != nil {
 		return
 	}
 	return
 }
 
 // UpdateStatus ..
-func (t *Content) UpdateStatus() (err error) {
-	if err = DBEngine.Model(&Content{}).Where(t.Name).Updates(t).Error; err != nil {
+func (t *Content) UpdateStatus(status Status) (err error) {
+	var txn = DBEngine.NewTransaction(true)
+	defer func() {
+		if err = txn.Commit(); err != nil {
+			return
+		}
+	}()
+
+	t.Status = status
+
+	var data []byte
+	if data, err = json.Marshal(*t); err != nil {
+		return
+	}
+
+	var entry = badger.NewEntry([]byte(t.Name), data)
+	if err = txn.SetEntry(entry); err != nil {
+		return
+	}
+
+	return
+}
+
+// GetContentByName ..
+func GetContentByName(name string) (content Content, err error) {
+	if err = DBEngine.View(func(txn *badger.Txn) (err error) {
+		var item *badger.Item
+		if item, err = txn.Get([]byte(name)); err != nil {
+			return
+		}
+		var data = make([]byte, item.ValueSize())
+		if _, err = item.ValueCopy(data); err != nil {
+			return
+		}
+		if err = json.Unmarshal(data, &content); err != nil {
+			return
+		}
+		return
+	}); err != nil {
 		return
 	}
 	return
 }
 
-type Status string
-
-const (
-	DoneStatus    Status = "done"
-	ErrorStatus   Status = "error"
-	PendingStatus Status = "pending"
-)
-
-var DBEngine *gorm.DB
-
-const DatabaseFile = "transfer.db"
-
-// Database ..
-func Database() (err error) {
-	if DBEngine, err = gorm.Open(sqlite.Open(DatabaseFile), &gorm.Config{}); err != nil {
+// GetContentByURL ..
+func GetContentByURL(url string) (content Content, err error) {
+	if err = DBEngine.View(func(txn *badger.Txn) (err error) {
+		var iter = txn.NewIterator(badger.DefaultIteratorOptions)
+		defer iter.Close()
+		for iter.Rewind(); iter.Valid(); iter.Next() {
+			var item = iter.Item()
+			var data = make([]byte, item.ValueSize())
+			if _, err = item.ValueCopy(data); err != nil {
+				return
+			}
+			if err = json.Unmarshal(data, &content); err != nil {
+				return
+			}
+			if content.URL == url {
+				return
+			}
+		}
+		return
+	}); err != nil {
 		return
 	}
-	if err = DBEngine.AutoMigrate(&Content{}); err != nil {
+	err = badger.ErrKeyNotFound
+	return
+}
+
+// GetContentsByStatus ..
+func GetContentsByStatus(status Status) (contents []Content, err error) {
+	if err = DBEngine.View(func(txn *badger.Txn) (err error) {
+		var iter = txn.NewIterator(badger.DefaultIteratorOptions)
+		defer iter.Close()
+		for iter.Rewind(); iter.Valid(); iter.Next() {
+			var item = iter.Item()
+			var data = make([]byte, item.ValueSize())
+			if _, err = item.ValueCopy(data); err != nil {
+				return
+			}
+			var content Content
+			if err = json.Unmarshal(data, &content); err != nil {
+				return
+			}
+			if content.Status == status {
+				contents = append(contents, content)
+			}
+		}
 		return
+	}); err != nil {
+		return
+	}
+	if len(contents) == 0 {
+		err = badger.ErrKeyNotFound
 	}
 	return
 }
 
-// GenName ..
+// GenName generate a unique name
 func GenName() (name string, err error) {
 	var hash = fnv.New64()
 again:
@@ -75,14 +188,12 @@ again:
 		return
 	}
 	name = hex.EncodeToString(hash.Sum(nil))
-	var task Content
-	if err = DBEngine.Where(&Content{Name: name}).First(&task).Error; err == gorm.ErrRecordNotFound {
+	if _, err = GetContentByName(name); err == badger.ErrKeyNotFound {
 		err = nil
 		return
 	} else if err != nil {
 		return
-	} else {
-		<-time.After(time.Microsecond * 100)
-		goto again
 	}
+	<-time.After(time.Microsecond * 100)
+	goto again
 }
--- go.mod
+++ go.mod
@@ -3,21 +3,26 @@ module transfer
 go 1.15
 
 require (
+	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/aliyun/aliyun-oss-go-sdk v2.1.4+incompatible
 	github.com/andybalholm/brotli v1.0.1 // indirect
 	github.com/baiyubin/aliyun-sts-go-sdk v0.0.0-20180326062324-cfa1a18b161f // indirect
 	github.com/cheggaaa/pb/v3 v3.0.5
 	github.com/dgraph-io/badger/v2 v2.2007.2
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gofiber/fiber/v2 v2.0.6
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
 	github.com/klauspost/compress v1.11.1 // indirect
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/mattn/go-sqlite3 v1.14.4 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/qiniu/api.v7/v7 v7.6.0
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/spf13/afero v1.4.1 // indirect
@@ -27,10 +32,10 @@ require (
 	github.com/spf13/viper v1.7.1
 	github.com/tosone/logging v1.1.0
 	github.com/unknwon/com v1.0.1
-	golang.org/x/sys v0.0.0-20201013132646-2da7054afaeb // indirect
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb // indirect
+	golang.org/x/sys v0.0.0-20201014080544-cc95f250f6bc // indirect
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
-	gorm.io/driver/sqlite v1.1.3
-	gorm.io/gorm v1.20.2
 )
